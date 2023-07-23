de603788618348dd73d9fbd91a4a97df88c81ced
Julian <jumpingafterrain@gmail.com>
2023-07-23 15:05:15 +0200

    scroll

--- game.c
+++ game.c
@@ -5,6 +5,7 @@
 #define GRAVITY 1.2f
 #define JUMP_VELOCITY -20.0f
 #define SPEED 8
+#define GROUND_LEVEL 50
 
 typedef struct {
   float x, y;
@@ -20,9 +21,11 @@ typedef struct {
 
 Engine engine;
 Player player;
-Platform platform;
+Platform platforms[3];
 int gameState;
-SDL_Color textColor = {255, 255, 255, 255};
+int scrollx;
+int scrolly;
+SDL_Color textColor;
 SDL_Texture* startTexture;
 
 void initGame() {
@@ -33,7 +36,11 @@ void initGame() {
   gameState = 2;  // Set to starting game state
   // graphics
   // start screen text
+  textColor = (SDL_Color){255, 255, 255, 255};
   startTexture = createTextTexture(&engine, "Press any key to start", textColor);
+  // scroll
+  scrollx = 0;
+  scrolly = 0;
 }
 
 void initPlayer() {
@@ -41,16 +48,32 @@ void initPlayer() {
   player.y = 100;
   player.vx = 0;
   player.vy = 0;
-  player.width = 50;
-  player.height = 50;
+  player.width = 20;
+  player.height = 20;
   player.onGround = 0;
 }
 
-void initPlatform() {
-  platform.x = 0;
-  platform.y = SCREEN_HEIGHT - 100;
-  platform.width = SCREEN_WIDTH;
-  platform.height = 100;
+void initPlatforms() {
+  Platform pf1, pf2, pf3;
+
+  pf1.x = 0;
+  pf1.y = SCREEN_HEIGHT - GROUND_LEVEL;
+  pf1.width = 300;
+  pf1.height = GROUND_LEVEL;
+
+  pf2.x = 400;
+  pf2.y = SCREEN_HEIGHT - 200;
+  pf2.width = 80;
+  pf2.height = 30;
+
+  pf3.x = 600;
+  pf3.y = SCREEN_HEIGHT - 300;
+  pf3.width = 80;
+  pf3.height = 30;
+
+  platforms[0] = pf1;
+  platforms[1] = pf2;
+  platforms[2] = pf3;
 }
 
 void handleInput() {
@@ -65,7 +88,7 @@ void handleInput() {
       // Start screen
       if (gameState == 2) {
         initPlayer();
-        initPlatform();
+        initPlatforms();
         gameState = 1;
         return;
       }
@@ -116,18 +139,37 @@ void handleInput() {
 }
 
 void update() {
+  // Update player
   player.vy += GRAVITY;
   player.x += player.vx;
   player.y += player.vy;
+  
+  player.onGround = 0; // Assume player is not on ground until we find a platform
+  // End game when fall
+  if (player.y > SCREEN_HEIGHT) {
+    gameState = 2;
+    return;
+  }
 
-  if (player.y + player.height > platform.y && 
-      player.x < platform.x + platform.width && 
-      player.x + player.width > platform.x) {
-    player.y = platform.y - player.height;
-    player.vy = 0;
-    player.onGround = 1;
-  } else {
-    player.onGround = 0;
+  // Scroll
+  scrollx = -player.x + (SCREEN_WIDTH / 2);
+  if (scrollx > 0) {
+    scrollx = 0;
+  }
+  scrolly = player.y - (SCREEN_HEIGHT / 2);
+  if (player.y > (SCREEN_HEIGHT / 2)) {
+    scrolly = 0;
+  }
+
+  // Platform collision
+  for (int i = 0; i < 3; i++) {
+    if (player.y + player.height > platforms[i].y && 
+        player.x < platforms[i].x + platforms[i].width && 
+        player.x + player.width > platforms[i].x) {
+      player.y = platforms[i].y - player.height;
+      player.vy = 0;
+      player.onGround = 1;
+    }
   }
 }
 
@@ -143,13 +185,16 @@ void render() {
       update(); 
 
 
-      SDL_Rect playerRect = {player.x, player.y, player.width, player.height};
+      SDL_Rect playerRect = {player.x + scrollx, player.y - scrolly, player.width, player.height};
       SDL_SetRenderDrawColor(engine.renderer, 255, 0, 0, 255);
       SDL_RenderFillRect(engine.renderer, &playerRect);
       
-      SDL_Rect platformRect = {platform.x, platform.y, platform.width, platform.height};
-      SDL_SetRenderDrawColor(engine.renderer, 0, 255, 0, 255);
-      SDL_RenderFillRect(engine.renderer, &platformRect);
+      for (int i = 0; i < 3; i++) {
+        SDL_Rect platformRect = {platforms[i].x + scrollx, platforms[i].y - scrolly, platforms[i].width, platforms[i].height};
+        SDL_SetRenderDrawColor(engine.renderer, 0, 255, 0, 255);
+        SDL_RenderFillRect(engine.renderer, &platformRect);
+
+      }
 
       SDL_RenderPresent(engine.renderer);
       break;
